53f59cd81c3335d24aed660463cb8340cc815f8a
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-05-12 22:40:07 +0300

    Added User entity by users table.

---
+++ src/main/java/com/jetbrains/model/UsersEntity.java
@@ -0,0 +1,76 @@
+package com.jetbrains.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "kotlin_webdemo", catalog = "")
+public class UsersEntity {
+    private int id;
+    private String email;
+    private String name;
+    private String picture;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "picture")
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (picture != null ? !picture.equals(that.picture) : that.picture != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (picture != null ? picture.hashCode() : 0);
+        return result;
+    }
+}
